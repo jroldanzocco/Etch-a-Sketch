fc40ffe7db279e1c4bf9f49b1d15643c84dc2ec7
Jeremias <jroldanzocco@gmail.com>
2022-05-31 09:51:55 -0300

    function hover() and leaves() for grid hovering

--- script.js
+++ script.js
@@ -5,9 +5,22 @@ function makeRows(rows, cols) {
   container.style.setProperty('--grid-cols', cols);
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
+    cell.addEventListener("mouseenter",hover)
+    cell.addEventListener("mouseleave", leaves)
     cell.innerText = (c + 1);
     container.appendChild(cell).className = "grid-item";
   };
 };
 
 makeRows(16, 16);
+
+function hover(){
+  console.log("hola")
+  this.classList.add("hover")
+}
+    
+function leaves(){
+    setTimeout(function(){
+    this.classList.remove('hover');
+    }.bind(this),5000)
+}
