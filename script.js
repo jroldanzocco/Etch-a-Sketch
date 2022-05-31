b7dcdae11d89bd8d9d4cf58f9c3027f8b571f3c4
Jeremias <jroldanzocco@gmail.com>
2022-05-31 15:54:44 -0300

    added logic for painting and fix

--- script.js
+++ script.js
@@ -1,26 +1,52 @@
 const container = document.querySelector("#container");
 
-function makeRows(rows, cols) {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
-  for (c = 0; c < (rows * cols); c++) {
+const clear = document.querySelector('.clear')
+function makeGrid(size) {
+  container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+  container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  for (let i = 0; i < (size * size); i++) {
     let cell = document.createElement("div");
-    cell.addEventListener("mouseenter",hover)
-    cell.addEventListener("mouseleave", leaves)
-    cell.innerText = (c + 1);
-    container.appendChild(cell).className = "grid-item";
+    cell.style.backgroundColor = "white";
+    container.appendChild(cell)
+    //hover
+    cell.addEventListener("mouseover",function(){
+      cell.style.backgroundColor = "black"
+    })
+    //reset grid
+    function clearGrid(){
+      clear.addEventListener("click",e =>{
+        cell.style.backgroundColor = "white"
+      })
+    }
+    clearGrid()
   };
 };
 
-makeRows(16, 16);
 
-function hover(){
-  console.log("hola")
-  this.classList.add("hover")
-}
-    
-function leaves(){
-    setTimeout(function(){
-    this.classList.remove('hover');
-    }.bind(this),5000)
-}
+makeGrid(16)
+
+
+const $sixteen = document.querySelector(".sixteen")
+$sixteen.addEventListener("click",function(e){
+  makeGrid(16)
+  e.preventDefault()
+})
+
+const $thirtytwo = document.querySelector(".threetwo")
+$thirtytwo.addEventListener("click",function(){
+  makeGrid(32)
+  e.preventDefault()
+})
+
+const $sixtyfour = document.querySelector(".sixfour")
+$sixtyfour.addEventListener("click",function(){
+  makeGrid(64)
+  e.preventDefault()
+})
+
+const $hundred = document.querySelector(".hundred")
+$hundred.addEventListener("click",function(){
+  makeGrid(100)
+  e.preventDefault()
+})
+
